738d8e727886437f6e3f9b56831870d1b853d501
J.MAR0V3 <67559377+JMarOve@users.noreply.github.com>
2025-01-08 23:12:11 +0000

    Time: 26 ms (5.17%), Space: 17.7 MB (23.65%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,3 +1,6 @@
+#Just a fancy solution using Lambda functions. Is it needed? Nop, but it is just cool.
+#Same solution can be achieved with a classic var=list(str(x)) 
+
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         var=list(filter(lambda i:(i),str(x)))
