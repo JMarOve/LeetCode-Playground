2f025b8f7b0dbce3e2cc57854173741ca3e082af
J.MAR0V3 <67559377+JMarOve@users.noreply.github.com>
2025-01-08 23:20:33 +0000

    Time: 8 ms (53%), Space: 18 MB (7.38%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,7 +1,18 @@
 #Just a fancy solution using Lambda functions. Is it needed? Nop, but it is just cool.
+#class Solution:
+   # def isPalindrome(self, x: int) -> bool:
+    #    var=list(filter(lambda i:(i),str(x)))
+     #   return(var==var[::-1])
 #Same solution can be achieved with a classic var=list(str(x)) 
 
+#Solution withouth converting it into a string. Really ugly. 
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        var=list(filter(lambda i:(i),str(x)))
-        return(var==var[::-1])
+        if x< 0:
+            return False
+        original_number=x
+        reverse_number = 0 
+        while original_number>0:
+            reverse_number=reverse_number*10+original_number%10
+            original_number=original_number//10 
+        return x==reverse_number
