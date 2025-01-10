c8c4e918a93bfca6b00dadf5fd1cfb0288a46686
J.MAR0V3 <67559377+JMarOve@users.noreply.github.com>
2025-01-10 23:54:24 +0000

    Time: 0 ms (100%), Space: 18.3 MB (33.11%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -1,13 +1,3 @@
 class Solution:
    def fizzBuzz(self, n: int) -> List[str]:
-        result = [None]*n
-        for i in range(1, n+1):
-            if i%3==0 and i%5==0:
-                result[i-1] = "FizzBuzz"
-            elif i%3==0:
-                result[i-1] = "Fizz"
-            elif i%5==0:
-                result[i-1] = "Buzz"
-            else:
-                result[i-1] = str(i)
-        return result
+        return[("Fizz"*(i%3==0)+"Buzz"*(i%5==0)) or str(i) for i in range(1,n+1)]
