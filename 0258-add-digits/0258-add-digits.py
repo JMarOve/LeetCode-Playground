d4f978f5ba0fa0f0d14ae55d1dd6a82858f55b9f
J.MAR0V3 <67559377+JMarOve@users.noreply.github.com>
2025-01-21 22:50:01 +0000

    Time: 4 ms (4.76%), Space: 18 MB (9.83%) - LeetHub

--- 0258-add-digits/0258-add-digits.py
+++ 0258-add-digits/0258-add-digits.py
@@ -1,6 +1,5 @@
-class Solution:
-    def addDigits(self, num: int) -> int:
-        s=list(str(num))
+'''Very mediocre solution 
+       s=list(str(num))
         print(s)
         res = 0
         while len(s)!=1:
@@ -9,3 +8,9 @@ class Solution:
             s = list(str(res))
             res = 0
         return int(s[0])
+        '''
+class Solution:
+
+    def addDigits(self, num: int) -> int:
+   
+        return 0 if num==0 else (1 +(num-1)%9)
